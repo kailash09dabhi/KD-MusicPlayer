e93a0db252bb633b6191ca5290cc9d150a64bf10
Kailash Dabhi <kailash09dabhi@gmail.com>
2016-11-23 20:25:53 +0530

    -raymenu basic implementation in allsongsFragment

--- mobile/src/main/java/com/kingbull/musicplayer/ui/main/categories/all/AllSongsFragment.java
+++ mobile/src/main/java/com/kingbull/musicplayer/ui/main/categories/all/AllSongsFragment.java
@@ -14,32 +14,53 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.kingbull.musicplayer.R;
 import com.kingbull.musicplayer.domain.Music;
 import com.kingbull.musicplayer.ui.base.BaseFragment;
 import com.kingbull.musicplayer.ui.base.PresenterFactory;
+import com.kingbull.musicplayer.ui.main.categories.all.raymenu.RayMenu;
 import com.kingbull.musicplayer.ui.songlist.SongsAdapter;
 import java.util.List;
 
 public final class AllSongsFragment extends BaseFragment<AllSongs.Presenter>
     implements LoaderManager.LoaderCallbacks<Cursor>, AllSongs.View {
+  private static final int[] ITEM_DRAWABLES = {
+      R.drawable.composer_button, R.drawable.composer_button_back, R.drawable.composer_button_queue,
+      R.drawable.composer_icn_search, R.drawable.composer_button_shuffle,
+      R.drawable.composer_button_multiselect
+  };
   @BindView(R.id.totalSongCountView) TextView totalSongCountView;
   @BindView(R.id.recyclerView) RecyclerView recyclerView;
+  @BindView(R.id.ray_menu) RayMenu rayMenu;
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_all_songs, null);
-    ButterKnife.bind(this,view);
-    setupView(view);
+    ButterKnife.bind(this, view);
+    setupView();
     return view;
   }
 
-  private void setupView(View v) {
+  private void setupView() {
     recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     getLoaderManager().initLoader(0, null, this);
+    final int itemCount = ITEM_DRAWABLES.length;
+    for (int i = 0; i < itemCount; i++) {
+      ImageView item = new ImageView(getActivity());
+      item.setImageResource(ITEM_DRAWABLES[i]);
+      final int position = i;
+      rayMenu.addItem(item, new View.OnClickListener() {
+
+        @Override public void onClick(View v) {
+          Toast.makeText(getActivity(), "position:" + position, Toast.LENGTH_SHORT).show();
+        }
+      });// Add a menu item
+    }
   }
 
   @Override public Loader<Cursor> onCreateLoader(int id, Bundle args) {
