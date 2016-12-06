1fd99d89adc4081a96923c1abbbdf696ad436b7b
Kailash Dabhi <kailash09dabhi@gmail.com>
2016-12-07 00:57:22 +0530

    - scroll thumb drawable added to make the scrolling beautiful

--- mobile/src/main/java/com/kingbull/musicplayer/RxBus.java
+++ mobile/src/main/java/com/kingbull/musicplayer/RxBus.java
@@ -4,18 +4,6 @@ import com.jakewharton.rxrelay2.PublishRelay;
 import com.jakewharton.rxrelay2.Relay;
 import io.reactivex.Observable;
 
-/**
- * Created with Android Studio.
- * User: ryan.hoo.j@gmail.com
- * Date: 6/25/16
- * Time: 3:01 PM
- * Desc: An EventBus powered by RxJava.
- * But before you use this RxBus, bear in mind this very IMPORTANT note:
- * - Be very careful when error occurred here, this can terminate the whole
- * event observer pattern. If one error ever happened, new events won't be
- * received because this subscription has be terminated after onError(Throwable).
- */
-
 public final class RxBus {
 
   private static final String TAG = "RxBus";
