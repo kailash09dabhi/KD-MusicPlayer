1ea67276a7c5c9b5ff24531665652287d8038300
Kailash Dabhi <kailash09dabhi@gmail.com>
2016-12-15 12:45:50 +0530

    - snackbar implementation

---
+++ mobile/src/main/java/com/kingbull/musicplayer/ui/Snackbar.java
@@ -0,0 +1,25 @@
+package com.kingbull.musicplayer.ui;
+
+import android.graphics.Color;
+import android.view.View;
+
+/**
+ * @author Kailash Dabhi
+ * @date 12/15/2016.
+ */
+
+public final class Snackbar {
+  private final View view;
+
+  public Snackbar(View view) {
+    this.view = view;
+  }
+
+  public void show(String message) {
+    android.support.design.widget.Snackbar snackbar =
+        android.support.design.widget.Snackbar.make(view, message,
+            android.support.design.widget.Snackbar.LENGTH_LONG);
+    snackbar.getView().setBackgroundColor(Color.parseColor("#99000000"));
+    snackbar.show();
+  }
+}
