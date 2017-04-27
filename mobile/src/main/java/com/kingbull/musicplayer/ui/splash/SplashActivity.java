c352b5594cebf9a159b29a4dfe1453ee5056ad47
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-04-28 00:21:41 +0530

    Request runtime permission to read/write storage & read phone state
    https://developer.android.com/training/permissions/requesting.html

--- mobile/src/main/java/com/kingbull/musicplayer/ui/splash/SplashActivity.java
+++ mobile/src/main/java/com/kingbull/musicplayer/ui/splash/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.kingbull.musicplayer.ui.splash;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import com.kingbull.musicplayer.ui.main.MainActivity;
 
@@ -11,10 +15,50 @@ import com.kingbull.musicplayer.ui.main.MainActivity;
  * @date 27 April, 2017 10:29 AM
  */
 public final class SplashActivity extends AppCompatActivity {
+  private static final int REQUEST_CODE_PERMISSION = 97;
+
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    askPermission();
+  }
+
+  private void askPermission() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this, new String[] {
+          Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+          Manifest.permission.READ_PHONE_STATE
+      }, REQUEST_CODE_PERMISSION);
+    } else {
+      gotoMainActivity();
+    }
+  }
+
+  private void gotoMainActivity() {
     Intent intent = new Intent(this, MainActivity.class);
     startActivity(intent);
     finish();
+  }
+
+  @Override public void onRequestPermissionsResult(int requestCode, String permissions[],
+      int[] grantResults) {
+    switch (requestCode) {
+      case REQUEST_CODE_PERMISSION: {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length == 3
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[1] == PackageManager.PERMISSION_GRANTED
+            && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+          gotoMainActivity();
+        } else {
+          finish();
+        }
+        return;
+      }
+    }
   }
 }
