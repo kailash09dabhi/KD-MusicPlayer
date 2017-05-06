bb45b505a4ad3b676b7c337daae0337a3b53caa2
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-05-06 16:48:45 +0530

    Add Application class for pro flavour
    Just get rid of MobileAds initialization code for pro flavour!

---
+++ mobile/src/pro/java/com.kingbull.musicplayer/MusicPlayerApp.java
@@ -0,0 +1,77 @@
+package com.kingbull.musicplayer;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatDelegate;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.stetho.Stetho;
+import com.kingbull.musicplayer.di.AppComponent;
+import com.kingbull.musicplayer.di.AppModule;
+import com.kingbull.musicplayer.di.DaggerAppComponent;
+import com.kingbull.musicplayer.di.StorageModule;
+import com.kingbull.musicplayer.player.MusicService;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * @author Kailash Dabhi
+ * @date 11/9/2016.
+ */
+public final class MusicPlayerApp extends Application {
+  private static MusicPlayerApp application;
+  private AppComponent appComponent;
+
+  public static MusicPlayerApp instance() {
+    return application;
+  }
+
+  public AppComponent component() {
+    return application.appComponent;
+  }
+
+  @Override public void onCreate() {
+    super.onCreate();
+    application = this;
+    Stetho.initializeWithDefaults(this);
+    if (BuildConfig.DEBUG) {
+      StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads()
+          .detectDiskWrites()
+          .detectNetwork()   // or .detectAll() for all detectable problems
+          .penaltyLog()
+          .build());
+      StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects()
+          .detectLeakedClosableObjects()
+          .penaltyLog()
+          .penaltyLog()
+          .build());
+    }
+    // Custom fonts
+    CalligraphyConfig.initDefault(
+        new CalligraphyConfig.Builder().setDefaultFontPath(getString(R.string.font_body))
+            .setFontAttrId(R.attr.fontPath)
+            .build());
+    injectDependencies();
+    startService(new Intent(this, MusicService.class));
+    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    AppEventsLogger.activateApp(this);
+  }
+
+  private void injectDependencies() {
+    appComponent = DaggerAppComponent.builder()
+        .appModule(new AppModule(this))
+        .storageModule(new StorageModule())
+        .build();
+  }
+
+  public String versionName() {
+    String versionName = "";
+    String packageName = getPackageName();
+    try {
+      versionName = getPackageManager().getPackageInfo(packageName, 0).versionName;
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+    return versionName;
+  }
+}
