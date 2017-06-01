d508444881b682d0ba8b5d7deb549a4d23328f9a
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-06-01 10:41:42 +0530

    Add fake AdmobBannerLoaded class for pro version

---
+++ mobile/src/pro/java/com.kingbull.musicplayer/ui.base.ads/AdmobBannerLoaded.java
@@ -0,0 +1,14 @@
+package com.kingbull.musicplayer.ui.base.ads;
+
+import android.view.ViewGroup;
+
+/**
+ * class expects that the Activity should have adView.
+ *
+ * @author Kailash Dabhi
+ * @date 10/3/2016.
+ */
+public final class AdmobBannerLoaded {
+  public AdmobBannerLoaded(ViewGroup rootView) {
+  }
+}
