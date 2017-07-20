8098e03254aa9ac74f8053ce97c3ac40d95ed42d
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-07-20 20:27:30 +0530

    Rename and set memory cache size of glide to 1/8th of max memory

--- mobile/src/main/java/com/kingbull/musicplayer/image/GlideAppModule.java
+++ mobile/src/main/java/com/kingbull/musicplayer/image/GlideAppModule.java
@@ -3,11 +3,16 @@ package com.kingbull.musicplayer.image;
 import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.GlideModule;
 import java.io.InputStream;
 
-public final class AlbumArtModule implements GlideModule {
+public final class GlideAppModule implements GlideModule {
   @Override public void applyOptions(Context context, GlideBuilder builder) {
+    final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+    // Use 1/8th of the available memory for this memory cache.
+    final int cacheSize = maxMemory / 8;
+    builder.setMemoryCache(new LruResourceCache(cacheSize));
   }
 
   @Override public void registerComponents(Context context, Glide glide) {
