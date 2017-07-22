78d4402331dad767ce52e6ed2e2a56c54e4a6576
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-07-22 23:00:38 +0530

    Don't open MainActivity on finish of MusicPlayer screen

--- mobile/src/main/java/com/kingbull/musicplayer/ui/music/MusicPlayerActivity.java
+++ mobile/src/main/java/com/kingbull/musicplayer/ui/music/MusicPlayerActivity.java
@@ -1,13 +1,11 @@
 package com.kingbull.musicplayer.ui.music;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.kingbull.musicplayer.MusicPlayerApp;
 import com.kingbull.musicplayer.ui.base.BaseActivity;
 import com.kingbull.musicplayer.ui.base.PresenterFactory;
-import com.kingbull.musicplayer.ui.main.MainActivity;
 
 /**
  * @author Kailash Dabhi
@@ -30,10 +28,5 @@ public final class MusicPlayerActivity extends BaseActivity<MusicPlayer.Presente
   }
 
   @Override protected void onPresenterPrepared(MusicPlayer.Presenter presenter) {
-  }
-
-  @Override public void finish() {
-    startActivity(new Intent(this, MainActivity.class));
-    super.finish();
   }
 }
