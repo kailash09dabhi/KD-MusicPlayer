44ee031745c5466c41299700c11b7479d60ff1be
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-07-27 13:20:49 +0530

    Add RxSchedulerRule to force execution on main thread only

---
+++ mobile/src/test/java/com/kingbull/musicplayer/RxSchedulerRule.java
@@ -0,0 +1,56 @@
+package com.kingbull.musicplayer;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.functions.Function;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.Callable;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * JUnit Test Rule which overrides RxJava and Android schedulers for use in unit tests.
+ * <p>
+ * All schedulers are replaced with Schedulers.trampoline().
+ */
+public class RxSchedulerRule implements TestRule {
+
+  private Scheduler SCHEDULER_INSTANCE = Schedulers.trampoline();
+
+  private Function<Scheduler, Scheduler> schedulerFunction = new Function<Scheduler, Scheduler>() {
+    @Override
+    public Scheduler apply(Scheduler scheduler) throws Exception {
+      return SCHEDULER_INSTANCE;
+    }
+  };
+
+  private Function<Callable<Scheduler>, Scheduler> schedulerFunctionLazy = new Function<Callable<Scheduler>, Scheduler>() {
+    @Override
+    public Scheduler apply(Callable<Scheduler> schedulerCallable) throws Exception {
+      return SCHEDULER_INSTANCE;
+    }
+  };
+
+  @Override
+  public Statement apply(final Statement base, Description description) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        RxAndroidPlugins.reset();
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(schedulerFunctionLazy);
+
+        RxJavaPlugins.reset();
+        RxJavaPlugins.setInitIoSchedulerHandler(schedulerFunctionLazy);
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(schedulerFunctionLazy);
+        RxJavaPlugins.setInitComputationSchedulerHandler(schedulerFunctionLazy);
+
+        base.evaluate();
+
+        RxAndroidPlugins.reset();
+        RxJavaPlugins.reset();
+      }
+    };
+  }
+}
